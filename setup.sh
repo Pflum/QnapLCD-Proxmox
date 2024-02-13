25ce86674f9b82192fa5834d1a0ca6de5bc59aca
DigitalFunk <57845261+DigitalFunk@users.noreply.github.com>
2024-02-12 23:26:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/sh
+
+if [ ${EUID} -ne 0 ]
+then
+  echo +++++++++++++++++++++++++
+  echo ++ MUST BE RUN AS ROOT ++
+  echo +++++++++++++++++++++++++
+  sleep 3
+	exit 1 # this is meant to be run as root
+fi
+
 curl -L https://github.com/pyserial/pyserial/tarball/master | tar xzf - --strip-components=1 --wildcards --no-anchored */serial
