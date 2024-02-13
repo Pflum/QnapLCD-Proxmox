df8040015941c6bd1b488f4ec2e1aa587f177697
DigitalFunk <57845261+DigitalFunk@users.noreply.github.com>
2024-02-13 00:01:22 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,4 +15,5 @@ cd /usr/local/bin/qnap_lcd
 git clone https://github.com/DigitalFunk/QnapLCD-Proxmox .
 curl -L https://github.com/pyserial/pyserial/tarball/master | tar xzf - --strip-components=1 --wildcards --no-anchored */serial
 cp lcd-menu.service /etc/systemd/system/
+systemctl daemon-reload
 systemctl enable --now lcd-menu.service
