f2c9c4646ff6aea4461e8f4c7ccda46f2fc54ad2
DigitalFunk <57845261+DigitalFunk@users.noreply.github.com>
2024-02-13 08:37:32 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,14 +6,19 @@ then
   echo ++ MUST BE RUN AS ROOT ++
   echo +++++++++++++++++++++++++
   sleep 3
-	exit 1 # this is meant to be run as root
+  exit 1 # this is meant to be run as root
 fi
 
-mkdir -p /usr/local/bin/qnap_lcd
-rm -rf /usr/local/bin/qnap_lcd/*
-cd /usr/local/bin/qnap_lcd
-git clone https://github.com/DigitalFunk/QnapLCD-Proxmox .
-curl -L https://github.com/pyserial/pyserial/tarball/master | tar xzf - --strip-components=1 --wildcards --no-anchored */serial
-cp lcd-menu.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable --now lcd-menu.service
+installdir=/usr/local/bin/qnap_lcd
+
+mkdir -p $installdir
+cd $installdir
+if [ $(pwd) == $installdir ]
+then
+    rm -rf $installdir/*
+    git clone https://github.com/DigitalFunk/QnapLCD-Proxmox .
+    curl -L https://github.com/pyserial/pyserial/tarball/master | tar xzf - --strip-components=1 --wildcards --no-anchored */serial
+    cp lcd-menu.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable --now lcd-menu.service
+fi
